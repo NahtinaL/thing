32245c64b58aa1861cdd8b16a0a1a9a13cfc356a
ChigaBiga <bohdan_bochulya@ukr.net>
2021-09-18 20:07:58 +0300

    project creation and DB schema creation

---
+++ src/test/java/com/strangethings/thing/DbManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strangethings.thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
